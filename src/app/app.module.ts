4cbe2b3836d7230b08f7aaa09bb2cd2193095381
piry1 <piry3943@gmail.com>
2017-12-20 00:17:22 +0100

    add register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -9,6 +9,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { CinemaComponent } from './cinema/cinema.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
@@ -17,12 +18,14 @@ import { CinemaComponent } from './cinema/cinema.component';
     NavBarComponent,
     FooterComponent,
     HomeComponent,
-    CinemaComponent
+    CinemaComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpModule
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../dbModel/user';
+import { DbService } from '../db.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor(private _db: DbService, private router: Router) { }
+
+  user: User = new User();
+  haslo: string;
+  passValid: string;
+  ngOnInit() {
+  }
+
+  register() {
+    console.log(this.user);
+    var user = this.user;
+
+    if (user.Imie != undefined && user.Nazwisko != undefined && user.Haslo != undefined && user.Login != undefined)
+      if (user.Imie != "" && user.Nazwisko != "" && user.Haslo != "" && user.Login != "") {
+        //this._db.addUser(user);
+        this.router.navigateByUrl('/cinema');
+      }
+  }
+
+  valuechange($event) {
+    if (this.user.Haslo.length <= this.haslo.length) {
+      if (this.user.Haslo == this.haslo)
+        this.passValid = "is-valid";
+      else
+        this.passValid = "is-invalid";
+    }
+    else
+      this.passValid = "";
+  }
+
+}
