8dfe7b560b536050fdfb963e091d9e32957d0d88
piry1 <piry3943@gmail.com>
2017-12-28 16:16:05 +0100

    Delete room

--- src/app/cinema/room/room.component.ts
+++ src/app/cinema/room/room.component.ts
@@ -15,6 +15,7 @@ export class RoomComponent implements OnInit {
 
   private newRoom: Room = new Room();
   private processing: boolean = false;
+  private deleteProcessing: boolean = false;
 
   ngOnInit() {
     this.getRooms();
@@ -30,11 +31,21 @@ export class RoomComponent implements OnInit {
   addRoom() {
     this.processing = true;
     console.log(this.newRoom);
-    this._db.addRoom(this.newRoom).subscribe(res => {
-      this.getRooms();
-      this.processing = false;
-    });
+    if (this.newRoom.Miejsca != undefined && this.newRoom.Nazwa != undefined && this.newRoom.Rzedy != undefined)
+      this._db.addRoom(this.newRoom).subscribe(res => {
+        this.getRooms();
+        this.processing = false;
+      });
 
+  }
+
+  deleteRoom(id: number) {
+    this.deleteProcessing = true;
+    console.log(id);
+    this._db.deleteRoom(id).subscribe(res => {
+      this.getRooms();
+      this.deleteProcessing = false;
+    });
   }
 
 }
