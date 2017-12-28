40f04d7fdd028eff9dd8ed93980b4cd4db4e1133
piry1 <piry3943@gmail.com>
2017-12-28 15:14:33 +0100

    Change tables to views in get request

--- apiScripts/CinemaApi/get.php
+++ apiScripts/CinemaApi/get.php
@@ -5,12 +5,12 @@ function getRequest($table, $key){
   $sql = "";
 
   switch ($table) {
-    case 'bilety': $sql ="select * from Bilety".($key?" WHERE id=$key":''); break;
+    case 'bilety': $sql ="select * from _Bilety_".($key?" WHERE id=$key":''); break;
     case 'filmy': $sql ="select * from Filmy".($key?" WHERE id=$key":''); break;
     case 'miejsca': $sql ="select * from Miejsca".($key?" WHERE id=$key":''); break;
     case 'rezerwacje': $sql ="select * from Rezerwacje".($key?" WHERE id=$key":''); break;
     case 'sale': $sql ="select * from Sale".($key?" WHERE id=$key":''); break;
-    case 'seanse': $sql ="select * from Seanse".($key?" WHERE id=$key":''); break;
+    case 'seanse': $sql ="select * from _Seanse_".($key?" WHERE id=$key":''); break;
     case 'bilet': $sql ="CALL WyswietlBilet('$key')"; break;
     case 'seansefilmu': $sql ="CALL WyswietlSeanse('$key')"; break;
     case 'wolnemiejsca': $sql ="CALL WyswietlMiejsca('$key')"; break;
