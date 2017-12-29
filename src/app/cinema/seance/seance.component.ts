75e6be957fd668c803cf7c8bdba97788988c22b1
piry1 <piry3943@gmail.com>
2017-12-29 12:36:42 +0100

    Add seance

--- src/app/cinema/seance/seance.component.ts
+++ src/app/cinema/seance/seance.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DbService } from '../../db.service';
+import { Seance } from '../../dbModel/seance';
 
 @Component({
   selector: 'app-seance',
@@ -10,10 +11,25 @@ export class SeanceComponent implements OnInit {
 
   private seances: any[] = [];
 
+  processing: boolean = false;
+  deleteProcessing: boolean = false;
+  submitError: boolean = false;
+
+  newSeance: Seance = new Seance();
+  films;
+  rooms;
+  selectedFilm;
+  selectedRoom;
+
+
   constructor(private _db: DbService) { }
+
+
 
   ngOnInit() {
     this.getSeances();
+    this.getFilms();
+    this.getRooms();
   }
 
   getSeances() {
@@ -21,6 +37,25 @@ export class SeanceComponent implements OnInit {
       this.seances = res;
       console.log(res);
     });
+  }
+
+  getFilms() {
+    this._db.getFilm()
+      .subscribe(res => this.films = res);
+  }
+
+  getRooms() {
+    this._db.getRoom()
+      .subscribe(res => this.rooms = res);
+  }
+
+  addSeance() {
+    this.processing = true;
+    this.submitError = false;
+
+    this._db.addSeance(this.newSeance)
+      .finally(() => this.processing = false)
+      .subscribe(res => this.getSeances(), err => this.submitError = true);
   }
 
 }
